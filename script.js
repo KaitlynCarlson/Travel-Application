8ac127262a03215bff4749b5445f06aca767ad00
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-01-11 00:02:38 -0800

    Application is now utilizing the entered locations within a POST call, in order to create a live flight price search session. It is returning, and console logging, the needed session key so that we can next utilize GET to see the session  results.

--- script.js
+++ script.js
@@ -51,6 +51,7 @@ $(document).ready(function() {
       //   console.log(userLocation);
       var userCityId = userresponse.Places[0].CityId;
       console.log(userCityId);
+
       var flightParameters = {
         async: true,
         crossDomain: true,
@@ -79,8 +80,24 @@ $(document).ready(function() {
         }
       };
 
-      $.ajax(flightParameters).done(function(flightData) {
-        // console.log(flightData);
+      $.ajax(flightParameters).done(function(test) {
+        console.log(test);
+      });
+      var sessionResults = {
+        async: true,
+        crossDomain: true,
+        url:
+          "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/d88a69b6-59fb-43bd-b1de-e986709ad487?pageIndex=0&pageSize=10",
+        method: "GET",
+        headers: {
+          "x-rapidapi-host":
+            "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+          "x-rapidapi-key": "54fe8ad1femshf879567efc0115ap19ca9fjsn5da4b88c683d"
+        }
+      };
+
+      $.ajax(sessionResults).done(function(test) {
+        console.log(test);
       });
     });
   }
