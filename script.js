2917f3524b3a9a4724b8e24e3a8d212dff7a00fa
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-01-11 13:40:47 -0800

    Flight query complete. Officially setting starting location, destination location, trip start date and trip end date to render relevant flight data to console.

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
   function buildQueryURLFlight() {
     var adventureLocation = $("#adventureLocationInput").val();
-    // console.log(adventureLocation);
-    var startFlightDate = $("#startDateInput").val();
-    var endFlightDate = $("#endDateInput").val();
     var userLocation = $("#userLocationInput").val();
     var cityId = "";
-
+    var startDate = $("#startDateInput").val();
+    var endDate = $("#endDateInput").val();
+    // console.log(adventureLocation);
     // console.log(userLocation);
     // console.log(startFlightDate);
     // console.log(endFlightDate);
@@ -28,8 +27,8 @@ $(document).ready(function() {
       var location = adventure.Places[0].PlaceName;
 
       //   console.log(location);
+
       cityId = adventure.Places[0].CityId;
-      console.log(cityId);
     });
 
     var userInfo = {
@@ -47,47 +46,22 @@ $(document).ready(function() {
     };
     $.ajax(userInfo).done(function(userresponse) {
       //   console.log(userresponse);
-
       //   console.log(userLocation);
       var userCityId = userresponse.Places[0].CityId;
       console.log(userCityId);
-
-      var flightParameters = {
+      console.log(cityId);
+      var flightQuote = {
         async: true,
         crossDomain: true,
         url:
-          "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0",
-        method: "POST",
-        headers: {
-          "x-rapidapi-host":
-            "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-          "x-rapidapi-key":
-            "54fe8ad1femshf879567efc0115ap19ca9fjsn5da4b88c683d",
-          "content-type": "application/x-www-form-urlencoded"
-        },
-        data: {
-          inboundDate: "2020-02-10",
-          cabinClass: "business",
-          children: "0",
-          infants: "0",
-          country: "US",
-          currency: "USD",
-          locale: "en-US",
-          originPlace: userCityId,
-          destinationPlace: cityId,
-          outboundDate: "2020-02-01",
-          adults: "1"
-        }
-      };
-
-      $.ajax(flightParameters).done(function(test) {
-        console.log(test);
-      });
-      var sessionResults = {
-        async: true,
-        crossDomain: true,
-        url:
-          "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/d88a69b6-59fb-43bd-b1de-e986709ad487?pageIndex=0&pageSize=10",
+          "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/" +
+          userCityId +
+          "/" +
+          cityId +
+          "/" +
+          startDate +
+          "?inboundpartialdate=" +
+          endDate,
         method: "GET",
         headers: {
           "x-rapidapi-host":
@@ -96,23 +70,18 @@ $(document).ready(function() {
         }
       };
 
-      $.ajax(sessionResults).done(function(test) {
-        console.log(test);
-        var key = test.SessionKey;
-        console.log(key);
+      $.ajax(flightQuote).done(function(flight) {
+        console.log(flight);
       });
     });
   }
 
   function buildQueryURLRest() {
+    var restStartLocation = $("#userInput");
     var travelBudget = $("#travelBudget").val();
     // console.log(travelBudget);
     var sleepBudget = $("#sleepBudget").val();
     // console.log(sleepBudget);
-    var startRestDate = $("#startDateInput").val();
-    var endRestDate = $("#endDateInput").val();
-    // console.log(startRestDate);
-    // console.log(endRestDate);
   }
 
   $("#searchCriteria").on("click", function() {
