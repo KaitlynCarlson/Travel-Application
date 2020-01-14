eb10a2ff244ef36d85d7e264137c2b922f955bd3
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-01-13 19:53:54 -0800

    Added modal on load. Played with style. Grabbing input values for user name and password.

--- user.js
+++ user.js
@@ -4,4 +4,11 @@ $(document).ready(function() {
     $("#myModal").modal("show");
     Cookies.set("popup", "popped");
   }
+
+  $("#user-account").on("click", function() {
+    var userName = $("#userName").val();
+    var userPassword = $("#userPassword").val();
+    console.log(userName);
+    console.log(userPassword);
+  });
 });
