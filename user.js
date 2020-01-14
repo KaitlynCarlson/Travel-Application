3d9b418a9f101398d682d1031a358590266c739a
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-01-14 13:02:05 -0800

    Updated HTML to reflect Remy changes

--- user.js
+++ user.js
@@ -1,14 +1,24 @@
+$("#user-account").on("click", function() {
+  var localAccounts = [];
+  const newUser = function(e) {
+    e.preventDefault();
+    var addUser = {
+      Name: $("#userName").val(),
+      Password: "#userPassword".val()
+    };
+    $(localAccounts).push($(addUser));
+  };
+
+  console.warn("added", localAccounts);
+
+  //   var test = $("#bottom-empty");
+  //   test.append(JSON.stringify(localAccounts));
+});
+
 $(document).ready(function() {
   $("#myModal").css("background-color", "darkslategrey");
   if (!Cookies.get("popup")) {
     $("#myModal").modal("show");
     Cookies.set("popup", "popped");
   }
-
-  $("#user-account").on("click", function() {
-    var userName = $("#userName").val();
-    var userPassword = $("#userPassword").val();
-    console.log(userName);
-    console.log(userPassword);
-  });
 });
