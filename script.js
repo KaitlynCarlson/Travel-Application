372140b85417bc8e5ee09a48380347f7b6327baf
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-01-18 16:36:07 -0800

    Weird bug fixes.

--- script.js
+++ script.js
@@ -103,6 +103,9 @@ function searchFlightPriceAndProvider(adventureFlight) {
         );
       }
     }
+    if (flightPrices === 0 || null) {
+      displayFlightTitle.append("Price quotes unavailable");
+    }
   }
   displayFlightTitle.append("Flights found for " + $("#startDateInput").val());
   displayFlightCard.append([displayFlightTitle, displayFlightBody]);
